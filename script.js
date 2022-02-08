d524824cd52f00d8c719bce9c515b54667242435
Alanov <ghstng@protonmail.ch>
2022-02-08 15:36:37 -0300

    buttons that show on display

--- script.js
+++ script.js
@@ -42,44 +42,52 @@ function receiveInput() {
     const btn0 = document.querySelector("#b0");
     btn0.addEventListener("click", () => {
         firstArray.push(0);
+        document.getElementById("display").textContent = firstArray.join("");
     });    
     const btn1 = document.querySelector("#b1");
     btn1.addEventListener("click", () => {
         firstArray.push(1);
+        document.getElementById("display").textContent = firstArray.join("");
     });    
     const btn2 = document.querySelector("#b2");
     btn2.addEventListener("click", () => {
         firstArray.push(2);
+        document.getElementById("display").textContent = firstArray.join("");
     });    
     const btn3 = document.querySelector("#b3");
     btn3.addEventListener("click", () => {
         firstArray.push(3);
+        document.getElementById("display").textContent = firstArray.join("");
     });    
     const btn4 = document.querySelector("#b4");
     btn4.addEventListener("click", () => {
         firstArray.push(4);
+        document.getElementById("display").textContent = firstArray.join("");
     }); 
     const btn5 = document.querySelector("#b5");
     btn5.addEventListener("click", () => {
         firstArray.push(5);
+        document.getElementById("display").textContent = firstArray.join("");
     });
     const btn6 = document.querySelector("#b6");
     btn6.addEventListener("click", () => {
         firstArray.push(6);
+        document.getElementById("display").textContent = firstArray.join("");
     });
     const btn7 = document.querySelector("#b7");
     btn7.addEventListener("click", () => {
         firstArray.push(7);
-        
         document.getElementById("display").textContent = firstArray.join("");
     });
     const btn8 = document.querySelector("#b8");
     btn8.addEventListener("click", () => {
         firstArray.push(8);
+        document.getElementById("display").textContent = firstArray.join("");
     });
     const btn9 = document.querySelector("#b9");
     btn9.addEventListener("click", () => {
         firstArray.push(9);
+        document.getElementById("display").textContent = firstArray.join("");
     });
 
 }
