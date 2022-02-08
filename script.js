10a494382f3081e0498c11a0f73d9170bcf947f5
Alanov <ghstng@protonmail.ch>
2022-02-08 15:56:43 -0300

    test in numbers buttons

--- script.js
+++ script.js
@@ -35,59 +35,110 @@ function operate(firstNumber, operator, secondNumber) {
 }
 function receiveInput() {
     let firstArray = [];
+    let secondArray = [];
     let firstNumber = 7;
     let secondNumber = 0;
-    let operator = "";
+    let operator = null;
 
     const btn0 = document.querySelector("#b0");
     btn0.addEventListener("click", () => {
-        firstArray.push(0);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(0);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(0);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });    
     const btn1 = document.querySelector("#b1");
     btn1.addEventListener("click", () => {
-        firstArray.push(1);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(1);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(1);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });    
     const btn2 = document.querySelector("#b2");
     btn2.addEventListener("click", () => {
-        firstArray.push(2);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(2);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(2);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });    
     const btn3 = document.querySelector("#b3");
     btn3.addEventListener("click", () => {
-        firstArray.push(3);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(3);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(3);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });    
     const btn4 = document.querySelector("#b4");
     btn4.addEventListener("click", () => {
-        firstArray.push(4);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(4);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(4);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     }); 
     const btn5 = document.querySelector("#b5");
     btn5.addEventListener("click", () => {
-        firstArray.push(5);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(5);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(5);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });
     const btn6 = document.querySelector("#b6");
     btn6.addEventListener("click", () => {
-        firstArray.push(6);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(6);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(6);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });
     const btn7 = document.querySelector("#b7");
     btn7.addEventListener("click", () => {
-        firstArray.push(7);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(7);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(7);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });
     const btn8 = document.querySelector("#b8");
     btn8.addEventListener("click", () => {
-        firstArray.push(8);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(8);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(8);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });
     const btn9 = document.querySelector("#b9");
     btn9.addEventListener("click", () => {
-        firstArray.push(9);
-        document.getElementById("display").textContent = firstArray.join("");
+        if(operator === null) {
+            firstArray.push(9);
+            document.getElementById("display").textContent = firstArray.join(""); 
+        } else {
+            secondArray.push(9);
+            document.getElementById("display").textContent = secondArray.join("");
+        }
     });
     const btnDivide = document.querySelector("#bDivide");
     btnDivide.addEventListener("click", () => {
@@ -104,6 +155,10 @@ function receiveInput() {
     const btnSubtract = document.querySelector("#bSubtract");
     btnSubtract.addEventListener("click", () => {
         operator = "-";
+    });
+    const btnEquals = document.querySelector("#bEquals");
+    btnEquals.addEventListener("click", () => {
+
     });
 }
 console.log(operate());
